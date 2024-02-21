1c2d154506d5e93f6c909a2d98c60433760002c9
TIASHA-MANDAL <tiashamandal21@gmail.com>
2024-02-22 00:36:28 +0530

    Changed by Tiasha

---
+++ Patient Panel/src/home page/ImageSlider.jsx
@@ -0,0 +1,31 @@
+// import React from 'react';
+// import { View, StyleSheet } from 'react-native';
+// import { SliderBox } from 'react-native-image-slider-box';
+
+// const ImageSlider = () => {
+//     const slides = [
+//         require("../../assets/images/lab3.jpg"),
+//         require("../../assets/images/lab1.jpeg"),
+//         require("../../assets/images/lab2.jpeg"),
+//     ];
+
+//     return (
+//         <View style={styles.imageSliderContainer}>
+//             <SliderBox
+//                 images={slides}
+//                 dotColor="#2A9988"
+//                 inactiveDotColor="#706767"
+//                 ImageComponentStyle={{ borderRadius: 15, width: "95%", marginTop: 15}}
+//             />
+//         </View>
+//     );
+// };
+
+// const styles = StyleSheet.create({
+//     imageSliderContainer: {
+//         flex: 1,
+//         alignItems: "center"
+//     }
+// });
+
+// export default ImageSlider;
