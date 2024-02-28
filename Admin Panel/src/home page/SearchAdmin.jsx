c9415ad30382a4c1810e6da7a0b9ddfb7adb6cc4
TIASHA-MANDAL <tiashamandal21@gmail.com>
2024-02-28 22:57:33 +0530

    TIASHA CHANGES WEDNESDAY

--- Admin Panel/src/home page/SearchAdmin.jsx
+++ Admin Panel/src/home page/SearchAdmin.jsx
@@ -1,42 +1,68 @@
+
 import React, { useState } from 'react';
-import { View, StyleSheet, TouchableOpacity, TextInput } from 'react-native';
+import { View, StyleSheet, TouchableOpacity, TextInput, Platform, Dimensions } from 'react-native';
+import { SafeAreaView } from 'react-native-safe-area-context';
 import { Octicons } from 'react-native-vector-icons';
 
 const SearchAdmin = () => {
     const [searchText, setSearchText] = useState('');
 
     return (
-        <View style={styles.searchContainer012}>
+        <View style={styles.searchContainer} nestedScrollEnabled>
             <TouchableOpacity>
-                <Octicons name="search" size={24} style={{ marginLeft: 15, padding: 5, color: "#706767" }} />
+                <Octicons name="search" size={24} style={styles.searchIcon} />
             </TouchableOpacity>
-            <View style={styles.searchWrapper012}>
+            <View style={styles.searchWrapper}>
                 <TextInput
-                    style={{ fontFamily: "regular02", width: "100%", height: "100%", paddingHorizontal: 12 }}
+                    style={styles.input}
                     value={searchText}
                     onChangeText={setSearchText}
                     placeholder="Search Coordinator"
                 />
             </View>
         </View>
+        
     );
 }
 
+const { width } = Dimensions.get('window');
 const styles = StyleSheet.create({
-    searchContainer012: {
+    searchContainer: {
         flexDirection: "row",
-        justifyContent: "center",
-        alignContent: "center",
-        backgroundColor: "#D1D1D6",
-        borderRadius: 8,
+        alignItems: "center",
+        backgroundColor: "#FFFFFF",
+        borderRadius: 10,
         marginVertical: 16,
+        marginHorizontal: width * 0.04,
+        borderWidth: 1.5,
+        borderColor: "#2A9988",
     },
-    searchWrapper012: {
+    searchIcon: {
+        marginLeft: 15,
+        padding: 10,
+        color: "#706767",
+    },
+    searchWrapper: {
         flex: 1,
-        backgroundColor: "#D1D1D6",
         marginRight: 12,
         borderRadius: 12,
-    }
+        padding: 2,
+    },
+    input: {
+        fontFamily: "regular02",
+        width: "100%",
+        ...Platform.select({
+            ios: {
+                height: 40, 
+                paddingHorizontal: 12,
+            },
+            android: {
+                height: 48,
+                paddingHorizontal: 16,
+            },
+        }),
+    },
 });
 
 export default SearchAdmin;
+
