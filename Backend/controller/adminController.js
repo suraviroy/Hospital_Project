e0e044e7254941b6717522950c25280b9759edc9
Thisisashutosh <thisisashutoshdubey@gmail.com>
2024-03-03 19:50:32 +0530

    added all registered patients route

--- Backend/controller/adminController.js
+++ Backend/controller/adminController.js
@@ -1,4 +1,4 @@
-import PatientSchema from '../model/patientSchema.js';
+import PatientSchema from "../model/patientSchema.js";
 
 export const patientregistration = async (req, res) => {
   try {
@@ -20,28 +20,27 @@ export const patientregistration = async (req, res) => {
       localContactRelation,
       localContactNumber,
       extistingPatientDiagnosis,
-    } = req.body
+    } = req.body;
 
     // Get current date in the specified format
-    const currentDate = new Date().toLocaleDateString('en-US', {
-      day: '2-digit',
-      month: 'long',
-      year: 'numeric'
-    })
+    const currentDate = new Date().toLocaleDateString("en-US", {
+      day: "2-digit",
+      month: "long",
+      year: "numeric",
+    });
 
     // Get current time in the specified format
-    const currentTime = new Date().toLocaleTimeString('en-US', {
-      hour: '2-digit',
-      minute: '2-digit',
-      hour12: true
-    })
+    const currentTime = new Date().toLocaleTimeString("en-US", {
+      hour: "2-digit",
+      minute: "2-digit",
+      hour12: true,
+    });
 
     const finduser = await PatientSchema.findOne({ patientId: patientId });
     if (finduser) {
-      console.log("user exist")
+      console.log("user exist");
       return res.status(200).json({ message: "ID already exists" });
-    }
-    else {
+    } else {
       const newUser = await PatientSchema.create({
         name,
         gender,
@@ -62,60 +61,52 @@ export const patientregistration = async (req, res) => {
         extistingPatientDiagnosis,
         date: currentDate,
         time: currentTime,
-        status: "Registered"
-      })
+        status: "Registered",
+      });
 
       res
         .status(200)
-        .json({ message: 'User created successfully', data: newUser })
+        .json({ message: "User created successfully", data: newUser });
     }
+  } catch (error) {
+    console.error("Error creating user:", error);
+    res.status(500).json({ error: "An error occurred while creating user" });
   }
-  catch (error) {
-    console.error('Error creating user:', error)
-    res.status(500).json({ error: 'An error occurred while creating user' })
-  }
-}
-
+};
 
 export const registeredPatientList = async (req, res) => {
   try {
     const registedPatients = await PatientSchema.find(
-      { status: "Registered" }, 
-      { name: 1, 
-        patientId: 1, 
-        image: 1, 
-        _id: 0 } 
+      { status: "Registered" },
+      { name: 1, patientId: 1, image: 1, _id: 0 }
     );
     res.status(200).json(registedPatients);
-
   } catch (err) {
     res.status(500).json({ message: err.message });
   }
-}
-
+};
 
 export const sectionAtodaysPatient = async (req, res) => {
   try {
-        
-    const currentDate = new Date().toLocaleDateString('en-US', {
-      day: '2-digit',
-      month: 'long',
-      year: 'numeric'
-    })
-   
+    const currentDate = new Date().toLocaleDateString("en-US", {
+      day: "2-digit",
+      month: "long",
+      year: "numeric",
+    });
+
     const registeredPatients = await PatientSchema.find(
-      { 
+      {
         status: "Registered",
-        date: currentDate 
-      }, 
-      { 
-        name: 1, 
-        patientId: 1, 
-        image: 1, 
-        gender:1,
+        date: currentDate,
+      },
+      {
+        name: 1,
+        patientId: 1,
+        image: 1,
+        gender: 1,
         age: 1,
         time: 1,
-        _id: 0 
+        _id: 0,
       }
     );
 
@@ -123,4 +114,55 @@ export const sectionAtodaysPatient = async (req, res) => {
   } catch (err) {
     res.status(500).json({ message: err.message });
   }
-}
+};
+
+export const sectionAallPatient = async (req, res) => {
+  try {
+    function parseTime(timeStr) {
+      const [time, period] = timeStr.split(" ");
+      const [hours, minutes] = time.split(":").map(Number);
+      let hourIn24Format = hours;
+      // Check if period exists and convert it to lowercase
+      const periodLowerCase = period ? period.toLowerCase() : null;
+      if (periodLowerCase === "pm" && hours !== 12) {
+        hourIn24Format += 12;
+      } else if (periodLowerCase === "am" && hours === 12) {
+        hourIn24Format = 0;
+      }
+      return hourIn24Format * 60 + minutes;
+    }
+
+    const registeredPatients = await PatientSchema.find(
+      {
+        status: "Registered",
+      },
+      {
+        name: 1,
+        date:1,
+        patientId: 1,
+        image: 1,
+        gender: 1,
+        age: 1,
+        time: 1,
+        _id: 0,
+      }
+    );
+
+    registeredPatients.sort((a, b) => {
+      const dateA = new Date(a.date);
+      const dateB = new Date(b.date);
+      if (dateA.getTime() !== dateB.getTime()) {
+        return dateA - dateB;
+      } else {
+        // If dates are same, sort by time
+        const timeA = parseTime(a.time);
+        const timeB = parseTime(b.time);
+        return timeA - timeB;
+      }
+    });
+
+    res.status(200).json(registeredPatients);
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+};
--- Backend/routes/adminRoute.js
+++ Backend/routes/adminRoute.js
@@ -1,10 +1,11 @@
 import express from 'express'
 
-import { patientregistration ,registeredPatientList,sectionAtodaysPatient } from '../controller/adminController.js'
+import { patientregistration ,registeredPatientList,sectionAallPatient,sectionAtodaysPatient } from '../controller/adminController.js'
 const adminRouter = express.Router()
 
 adminRouter.post('/patientregistration', patientregistration);
 adminRouter.get('/registeredPatientList', registeredPatientList);
 adminRouter.get('/sectionAtodaysPatient', sectionAtodaysPatient);
+adminRouter.get('/sectionAallPatient', sectionAallPatient);
 
 export default adminRouter
