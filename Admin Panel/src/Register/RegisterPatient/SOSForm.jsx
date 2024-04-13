a78987693e30ac2fcabb48ffc7b76772db4dd6ac
TIASHA-MANDAL <91721067+TIASHA-MANDAL@users.noreply.github.com>
2024-04-13 09:28:23 +0530

    SOSForm.jsx

--- Admin Panel/src/Register/RegisterPatient/SOSForm.jsx
+++ Admin Panel/src/Register/RegisterPatient/SOSForm.jsx
@@ -1,59 +1,55 @@
-import React, { useState } from 'react';
-import { Text, View, Dimensions, StyleSheet, TouchableOpacity, Image } from 'react-native';
-const windowWidth = Dimensions.get('window').width;
-// import { Color } from '../../../GlobalStyles';
+
+import React from 'react';
+import { View, Dimensions } from 'react-native';
 import { SelectList } from 'react-native-dropdown-select-list';
-import FontAwesome from 'react-native-vector-icons/FontAwesome5';
 
+const windowWidth = Dimensions.get('window').width;
 
-const SOSForm = () => {
-  const [selected, setSelected] = useState([]);
+const SOSForm = ({ selectedOption, setSelectedOption, isClicked, setIsClicked }) => {
+    const Dis_Options = [
+        {
+            value: 'Somewhat sick',
+            key: 'SOS',
+        },
+        {
+               value: 'Sick',
+                key: 'SIC',
+              },
+              {
+                value: 'Quite sick',
+                key: 'QUS',
+              },
+              {
+                value: 'Very sick',
+                key: 'VRS',
+              },
+              {
+                value: 'Morbus',
+                key: 'MRB',
+              },
+    ];
 
-  const Dis_Options = [
-    {
-      value: 'Somewhat sick',
-      key: 'SOS',
-    },
-    {
-      value: 'Sick',
-      key: 'SIC',
-    },
-    {
-      value: 'Quite sick',
-      key: 'QUS',
-    },
-    {
-      value: 'Very sick',
-      key: 'VRS',
-    },
-    {
-      value: 'Morbus',
-      key: 'MRB',
-    },
-  ]
-
-  return (
-    <View style={{ paddingHorizontal: 10 }}>
-      <SelectList
-        setSelected={(val) => setSelected(val)}
-        data={Dis_Options}
-        save="value"
-        // label="Selected Diseases"
-        placeholder='Select'
-        searchPlaceholder='Search'
-        boxStyles={{
-          borderRadius: 5,
-          borderWidth: 0.5,
-          borderColor: '#A99F9F',
-          marginTop: windowWidth * 0.03,
-          backgroundColor: '#e3e3e3',
-          paddingLeft: 15,
-          paddingRight: 15,
-        }}
-        inputStyles={{ color: '#8E7D7D', fontSize: 15 }}
-      />
-    </View>
-  )
+    return (
+        <View style={{ paddingHorizontal: 10 }}>
+            <SelectList
+                setSelected={(val) => setSelectedOption(val)}
+                data={Dis_Options}
+                save="value"
+                placeholder='Select'
+                searchPlaceholder='Search'
+                boxStyles={{
+                    borderRadius: 5,
+                    borderWidth: 0.5,
+                    borderColor: '#A99F9F',
+                    marginTop: windowWidth * 0.03,
+                    backgroundColor: '#e3e3e3',
+                    paddingLeft: 15,
+                    paddingRight: 15,
+                }}
+                inputStyles={{ color: '#8E7D7D', fontSize: 15 }}
+            />
+        </View>
+    );
 };
 
 export default SOSForm;
